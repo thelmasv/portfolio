147dfbc41fa028406156b355b588eac8623b158e
thelmasv <43722195+thelmasv@users.noreply.github.com>
2021-02-05 22:12:32 +0100

    removed other folders

--- Ny/script.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-   $(window).scroll(function () {
-      if ($(this).scrollTop() > 50) {
-         $(‘body’).addClass(‘changeColor’)
-      } else {
-         $(‘body’).removeClass(‘changeColor’)
-      }
-   });
-});
